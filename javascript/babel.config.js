cce039d1c7f76e05bb39c3a330495fd2762aef8b
Izzuddin Ahmad Afif <izzuddinafif@gmail.com>
2025-10-14 14:56:33 +0700

    chore: Remove old top-level directories

--- javascript/babel.config.js
+++
@@ -1,7 +0,0 @@
-// CommonJS format
-module.exports = {
-    presets: [
-      ['@exercism/babel-preset-javascript', { corejs: '3.39' }],
-    ],
-  };
-  
--- javascript/fruit-picker/notifier.js
+++
@@ -1,8 +0,0 @@
-/**
- * @param {FruitPickerSuccess | FruitPickerError} message
- * @return void
- */
-export function notify(message) {
-  // This is a mocked function for use with the exercise.
-  message;
-}
